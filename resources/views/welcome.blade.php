160fe719d59e50d7458214439b669a821e11a444
Pavel Katunin <katunin.pavel@gmail.com>
2020-12-02 16:42:34 +0300

    v1.1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,8 +24,18 @@ fixtome - мобильные исследования
         </div>
         <div class="button-block">
             <a class="button new-research-button" href="newResearch">Создать опрос</a>
-            {{-- <a href="about">Как это работает?</a> --}}
+            <button class="how-it-works" onclick="howItWorks()">Как это работает?</button>
         </div>
+    </div>
+    <div class="video">
+        <video controls width="400" height="300" muted>
+            {{-- <source src="video.mp4" type="video/mp4"><!-- MP4 для Safari, IE9, iPhone, iPad, Android, и Windows Phone 7 --> --}}
+            <source src="viseo/howitworks.webm" type="video/webm"><!-- WebM/VP8 для Firefox4, Opera, и Chrome -->
+            {{-- <source src="video.ogv" type="video/ogg"><!-- Ogg/Vorbis для старых версий браузеров Firefox и Opera --> --}}
+            {{-- <object data="video.swf" type="application/x-shockwave-flash"><!-- добавляем видеоконтент для устаревших браузеров, в которых нет поддержки элемента video --> --}}
+            {{-- <param name="movie" value="video.swf"> --}}
+            </object>
+        </video>
     </div>
 
 </div>
